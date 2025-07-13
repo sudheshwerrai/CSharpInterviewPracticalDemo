22e8bcf02aa8d38f221e970e541ad65fc59e859c
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2025-07-13 15:20:39 +0530

    NullAndCoalescingOperator in C#

---
+++ CSharpInterviewPractical/NullAndCoalescingOperator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpInterviewPractical
+{
+    class NullAndCoalescingOperator
+    {        
+        static void Main()
+        {                  
+            int? age = null;
+
+            if (age.HasValue)
+                Console.WriteLine($"Age: {age}");
+            else
+                Console.WriteLine("Age: Not provided");
+
+            int? inputAge = null;
+            int finalAge = inputAge ?? 18;
+            Console.WriteLine(finalAge);
+        }
+    }
+}
