5bcd2e7c578c1ae61e5a960b4fa14cedce7b86c0
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2025-08-09 07:38:48 +0530

    Private Constructor in C#

---
+++ CSharpInterviewPractical/PrivateConstructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+//Private Constructor IN C#
+public class Singleton
+{   
+    private static Singleton _instance;
+    private static int counter = 0;
+
+    private Singleton()
+    {
+        counter++;
+        Console.WriteLine("Instance Created:" + counter);
+    }
+
+    public static Singleton GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Singleton();
+        }
+        return _instance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        //Singleton obj1 = new Singleton();
+        //Singleton obj2 = new Singleton();
+
+        Singleton obj1 = Singleton.GetInstance();
+        Singleton obj2 = Singleton.GetInstance();
+
+        Console.WriteLine(object.ReferenceEquals(obj1, obj2)); 
+    }
+}
