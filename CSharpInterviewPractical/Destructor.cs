ec8313cb3979833e05acdfda11652d3db052947c
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2025-08-13 17:42:11 +0530

    Destructor In C#

---
+++ CSharpInterviewPractical/Destructor.cs
@@ -0,0 +1,46 @@
+﻿namespace CSharpInterviewPractical
+{
+    using System;
+    using System.IO;
+
+    class FileHandler
+    {
+        private StreamWriter writer;
+        private string fileName;
+
+        public FileHandler(string fileName)
+        {
+            this.fileName = fileName;
+            writer = new StreamWriter(fileName, true);
+            Console.WriteLine("File opened: " + fileName);
+        }
+
+        public void WriteData(string data)
+        {
+            writer.WriteLine(data);
+            Console.WriteLine("Data written: " + data);
+        }
+
+        ~FileHandler()
+        {
+            if (writer != null)
+            {
+                writer.Close();
+                Console.WriteLine("File closed: " + fileName);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            FileHandler fh = new FileHandler($"DestructorExample.txt");
+            fh.WriteData("Destructor example");
+
+            Console.WriteLine("Objects created. Destructor will be called automatically by GC later.");
+            Console.ReadLine();
+        }
+    }
+}
