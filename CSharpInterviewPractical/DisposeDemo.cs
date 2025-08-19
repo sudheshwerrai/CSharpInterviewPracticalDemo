ad0354f329908a9e6e8c16df9bb3a99b724106e7
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2025-08-19 17:40:22 +0530

    Finalize And Dispose In C#

---
+++ CSharpInterviewPractical/DisposeDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace DisposeDemo
+{
+    class DisposeExample : IDisposable
+    {
+        private FileStream fs;
+
+        public DisposeExample(string path)
+        {
+            fs = new FileStream(path, FileMode.Create);
+            Console.WriteLine("Constructor: FileStream created");
+        }
+
+        public void Dispose()
+        {
+            fs.Close();
+            Console.WriteLine("Dispose: FileStream closed");
+        }
+
+        static void Main()
+        {
+            using (var obj = new DisposeExample("demo.txt"))
+            {
+                Console.WriteLine("Using block: Working with the object");
+            }
+
+            Console.WriteLine("Main method completed");
+        }
+    }
+}
---
+++ CSharpInterviewPractical/FinalizeDemo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FinalizeDemo
+{
+    class FinalizeExample
+    {
+        public FinalizeExample()
+        {
+            Console.WriteLine("Constructor: FinalizeExample object created");
+        }
+
+        ~FinalizeExample()
+        {
+            Console.WriteLine("Finalizer: FinalizeExample object is being finalized");
+        }
+
+        static void CreateObject()
+        {
+            FinalizeExample obj = new FinalizeExample();
+        }
+
+        static void Main()
+        {
+            CreateObject();
+            GC.Collect(); // Force garbage collection
+            GC.WaitForPendingFinalizers(); // Wait for finalizer to run
+
+            Console.WriteLine("Main method completed");
+        }
+    }
+
+
+}
