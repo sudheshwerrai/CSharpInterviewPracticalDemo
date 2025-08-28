92d4ce1d5f5b0f18068ebe79c62c92a672f5f59f
Sudheshwer Rai <sudheshwer.tech@gmail.com>
2025-08-28 17:08:56 +0530

    Var Keyword In C#

---
+++ CSharpInterviewPractical/VarKeyword.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using System.Collections;
+
+namespace CSharpInterviewPractical
+{
+    class Program
+    {
+
+        int i = 10;
+        double j = 20.4;
+        char k = 'C';
+        bool l = true;
+        string m = "Test";
+        object n = null;
+
+        static void Main()
+        {
+            //Var Keyword in C#
+           
+
+            var program = new Program();
+            var o = 10;
+
+            var arrayList = new ArrayList();
+            arrayList.Add(10);
+            arrayList.Add("Ten");
+            arrayList.Add(10.00);
+            arrayList.Add(true);
+            arrayList.Add('C');
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
